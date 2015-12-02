1bca53e39bb8a037543c646f8b517689a13394a6
André Taiar <taiar@hotmail.com>
2015-12-02 10:47:18 -0200

    Controller das consultas.

--- src/Controladores/ConsultasController.java
+++ src/Controladores/ConsultasController.java
@@ -4,6 +4,9 @@ import Entidades.Atores.Medico;
 import Entidades.Atores.Paciente;
 import Entidades.Atores.Recepcionista;
 import Entidades.Clinico.Consulta;
+import Entidades.Clinico.Exame;
+import Entidades.Clinico.Prontuario.DadosProntuario;
+import Entidades.Clinico.Prontuario.Prontuario;
 
 import java.util.GregorianCalendar;
 
@@ -21,9 +24,43 @@ public class ConsultasController {
         return consulta;
     }
 
-    public Consulta atenderConsulta(Consulta consulta) {
+    public Consulta atenderConsulta(Consulta consulta, DadosProntuario dadosProntuario) {
         consulta.setAconteceu(true);
-
+        consulta.setDados(dadosProntuario);
+        this.associaConsultaProntuario(consulta.getAtendido().getHistorico(), consulta);
         return consulta;
     }
+
+    private Prontuario associaConsultaProntuario(Prontuario prontuario, Consulta consulta) {
+        prontuario.addConsulta(consulta);
+        return prontuario;
+    }
+
+    public static DadosProntuario preencheDadosProntuario(String evolucaoMedicaDiaria, String
+            evolucaoEnfermagemEOutrasEspecialidades, String raciocinioMedico, String hipotesesDiagnosticoDefinitivo,
+            String condutaTerapeutica, String prescricoesMedicas, String descricoesCirurgicas, String fichasAnestesicas,
+            String resumoDeAlta, String atendimentoAmbulatorialUrgencia, String observacaoBoletinsMedicos) {
+
+        DadosProntuario dados = new DadosProntuario();
+
+        dados.setEvolucaoMedicaDiaria(evolucaoMedicaDiaria);
+        dados.setEvolucaoEnfermagemEOutrasEspecialidades(evolucaoEnfermagemEOutrasEspecialidades);
+        dados.setRaciocinioMedico(raciocinioMedico);
+        dados.setHipotesesDiagnosticoDefinitivo(hipotesesDiagnosticoDefinitivo);
+        dados.setCondutaTerapeutica(condutaTerapeutica);
+        dados.setPrescricoesMedicas(prescricoesMedicas);
+        dados.setDescricoesCirurgicas(descricoesCirurgicas);
+        dados.setFichasAnestesicas(fichasAnestesicas);
+        dados.setResumoDeAlta(resumoDeAlta);
+        dados.setAtendimentoAmbulatorialUrgencia(atendimentoAmbulatorialUrgencia);
+        dados.setObservacaoBoletinsMedicos(observacaoBoletinsMedicos);
+
+        return dados;
+    }
+
+    public static DadosProntuario adicionaExameDadosProntuario(Exame exame, DadosProntuario dados) {
+        dados.getExamesGerais().add(exame);
+        return dados;
+    }
+
 }
